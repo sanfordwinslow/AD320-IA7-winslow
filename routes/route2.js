d7cd427534e22e654e66be07014cb3c303df7bd3
Sanford Winslow <sanfordwinslow@Sanfords-MacBook-Pro.local>
2019-03-02 18:35:43 -0800

    made it work

--- routes/route2.js
+++ routes/route2.js
@@ -3,11 +3,25 @@ var router = express.Router();
 var object = require("../colors.json");
 
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
+  res.send(object);
+});*/
+
+router.get('/all', function(req, res, next)  {
   res.send(object);
 });
 
+router.get('/:color', function(req, res, next) {
+  res.send(colorName(req.params.color));
+});
+
+function colorName(colorString) {
+  for(var i = 0; i < object.colors.length; i++) {
+    if(object.colors[i].color === colorString) {
+      return object.colors[i];
+    };
+  };
+};
 
 module.exports = router;
